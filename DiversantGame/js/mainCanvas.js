1eec5e9ae8561a27e97226581667a662e76f9f57
shtolce <kimdimka@yandex.ru>
2019-01-27 16:44:21 +1000

    1231

--- DiversantGame/js/mainCanvas.js
+++ DiversantGame/js/mainCanvas.js
@@ -5,6 +5,8 @@ var xmlNPCPlayer;
 var PlayerFwdSprites=[];
 var playerImage;
 var NPCImage;
+var NPCImageBWD=new Map();
+var NPCImageFWD=new Map();
 var playerImageBwd;
 var PlayerSprites=[];
 var NPCSprites=[];
@@ -28,28 +30,12 @@ function writePlayerObj(){
         }
     });
 }
-function writeNPCObj(){
-    var xmlAnim = xmlNPCPlayer.getElementsByTagName("animation")[0];
-    var xmlCur=xmlAnim.childNodes;
-    xmlCur.forEach((el,i,xmlCur)=> {
-        if (el.nodeName=="cut"){
-            var NPCSpriteObj={
-                dom:NPCImage,
-                width:el.attributes['w'].value,
-                x:el.attributes['x'].value,
-                y:el.attributes['y'].value,
-                height:el.attributes['h'].value,
-                npcXPos:700
-            };
-            NPCSprites.push(NPCSpriteObj);
-        }
-    });
-}
+
 function reqReadyStateChange() {
     if (request.readyState == 4) {
         var status = request.status;
         if (status == 200) {
-           xmlPlayerFwd=request.responseXML;
+            xmlPlayerFwd=request.responseXML;
             writePlayerObj();
         } else {
         }
@@ -61,21 +47,54 @@ function reqReadyStateChangeNPC() {
         var status = requestNPC.status;
         if (status == 200) {
             xmlNPCPlayer=requestNPC.responseXML;
-            writeNPCObj();
+            writeNPCObj('Seller');
         } else {
         }
     }
 }
-request.open("GET", "img/playerFWD.xml");
-request.onreadystatechange = reqReadyStateChange;
-request.send();
-
-requestNPC.open("GET", "img/Seller.xml");
-requestNPC.onreadystatechange = reqReadyStateChangeNPC;
-requestNPC.send();
 
 
+window.addEventListener('load', function() {
+    NPCImageFWD.set('Seller',document.createElement('img'));
+    NPCImageFWD.get('Seller').src="img/SellerSpriteFWD.png";
+    NPCImageBWD.set('Seller',document.createElement('img'));
+    NPCImageBWD.get('Seller').src="img/SellerSpriteBWD.png";
+    playerImageFwd=document.createElement('img');
+    playerImageFwd.src="img/playerSprite1.png";
+    playerImageFwd.onload=function(){
+        var id = window.requestAnimFrame(gameLoop);
+    };
+    playerImageBwd=document.createElement('img');
+    playerImageBwd.src="img/playerSprite2.png";
 
+    request.open("GET", "img/playerFWD.xml");
+    request.onreadystatechange = reqReadyStateChange;
+    request.send();
+
+    requestNPC.open("GET", "img/Seller.xml");
+    requestNPC.onreadystatechange = reqReadyStateChangeNPC;
+    requestNPC.send();
+
+});
+function writeNPCObj(npcInstance){
+    var xmlAnim = xmlNPCPlayer.getElementsByTagName("animation")[0];
+    var xmlCur=xmlAnim.childNodes;
+    xmlCur.forEach((el,i,xmlCur)=> {
+        if (el.nodeName=="cut"){
+            var NPCSpriteObj={
+                domFWD:NPCImageFWD.get(npcInstance),
+                domBWD:NPCImageBWD.get(npcInstance),
+                dom:NPCImage,
+                width:el.attributes['w'].value,
+                x:el.attributes['x'].value,
+                y:el.attributes['y'].value,
+                height:el.attributes['h'].value,
+                npcXPos:700
+            };
+            NPCSprites.push(NPCSpriteObj);
+        }
+    });
+}
 var canvas=document.getElementById("mainCanvas");
 var canvasCtx = canvas.getContext("2d");
 var backgroundImg = new Image(totalSceneWidth,769);
@@ -108,9 +127,10 @@ function drawNPC(npcNumber,i1){
     var y1=NPCSprites[i1].y;
     var width1 =NPCSprites[i1].width;
     var height1 =NPCSprites[i1].height;
-    NPCSprites[i1].dom=NPCImage;
     var npcX=NPCSprites[i1].npcXPos-(x*totalSceneWidth/visibleSceneWidth);
-    canvasCtx.drawImage(NPCSprites[i1].dom,x1,y1,width1,height1, npcX,560,width1,height1);
+
+    if (NPCSprites[i1].domFWD)
+        canvasCtx.drawImage(NPCSprites[i1].domFWD,x1,y1,width1,height1, npcX,560,width1,height1);
 }
 
 function drawBackGround(x){
@@ -170,7 +190,6 @@ function movePlayerBWD(){
     direction=2;
 }
 
-
 function gameLoop() {
    drawBackGround(x);
    drawNPC(1,Math.floor(iNpc/64));
@@ -188,17 +207,6 @@ function gameLoop() {
    };
     window.requestAnimFrame(gameLoop);
 }
-window.addEventListener('load', function() {
-    playerImageFwd=document.createElement('img');
-    playerImageFwd.src="img/playerSprite1.png";
-    playerImageFwd.onload=function(){
-        var id = window.requestAnimFrame(gameLoop);
-    };
-    playerImageBwd=document.createElement('img');
-    playerImageBwd.src="img/playerSprite2.png";
-    NPCImage=document.createElement('img');
-    NPCImage.src="img/SellerSprite.png";
-});
 window.addEventListener('keydown',function(ev){
     keyDown = ev.keyCode;
 });
