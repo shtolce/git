0be8e45c50fd99f4b8ba56dbf44874fe432a080f
shtolce <kimdimka@yandex.ru>
2019-02-16 21:11:05 +1000

    В сервисе добавил заднефоновую асинхронную задачу меняющую диалоги даже когда к серверу не обращаются

--- DivGameApi/DivGameApi/Services/DialogService.cs
+++ DivGameApi/DivGameApi/Services/DialogService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using DivGameApi.Models;
 namespace DivGameApi.Services
@@ -8,12 +9,29 @@ namespace DivGameApi.Services
     public class DialogService
     {
         public Dialog1 dialogText { get; set; }
+        public async void Upd()
+        {
+                await Task.Run(() =>
+                {
+                    int i = 0;
+                    while (true)
+                    {
+                        i++;
+                        dialogText.Text = "fuck off" +i.ToString();
+                        Thread.Sleep(3000);
+                        if (i > 10000) i = 0;
+                    }
+                });
+        }
         public DialogService()
         {
             Dialog1 dialog = new Dialog1();
             dialog.PersonId = "1";
             dialog.Text = "fuck off";
             dialogText = dialog;
+            Upd();   
+
+
         }
 
 
